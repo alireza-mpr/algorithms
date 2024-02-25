f0ac39a2532ab49abee367321c2b7b1e2f2aba54
Alireza Mokhtaripour <alireza.mokhtaripour@gmail.com>
2024-02-25 13:15:22 +1100

    Added Sorting Algorithms

---
+++ Sorting Algorithms/SortingAlgorithms/BubbleSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class BubbleSort
+    {
+        public static void Sort(int[] array)
+        {
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    var left = array[i];
+                    var right = array[i + 1];
+                    if (left > right)
+                    {
+                        array[i] = right;
+                        array[i + 1] = left;
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+
+        public static void SortSlightlyOptimized(int[] array)
+        {
+            int wall = array.Length - 1; // https://youtu.be/9I2oOAr2okY?si=yEYpXAh372rISLct
+            do
+            {
+                for (int i = 0; i < wall; i++)
+                {
+                    var left = array[i];
+                    var right = array[i + 1];
+                    if (left > right)
+                    {
+                        array[i] = right;
+                        array[i + 1] = left;
+                    }
+                }
+                wall--;
+            } while (wall >= 0);
+        }
+    }
+}
---
+++ Sorting Algorithms/SortingAlgorithms/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 10, 3, 5, 7, 3, 1, -1, 2, 0 };
+
+            BubbleSort.SortSlightlyOptimized(array);
+
+            array.ToList().ForEach(p => Console.Write(p + " "));
+        }
+    }
+}
