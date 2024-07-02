b5ed34160251cc4ba2f94e141164fd0f1427eeab
Alireza Mokhtaripour <alireza.mokhtaripour@gmail.com>
2024-07-02 20:23:50 +1000

    Added N-element sum algorithm

---
+++ Arrays/ArrayAlgorithms/NElementSum.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayAlgorithms
+{
+    public class NElementSum
+    {
+        public static List<int[]> FindElements(int N, int[] array, int targetSum)
+        {
+            Array.Sort(array);
+            return FindElements(N, 0, array, targetSum);
+        }
+
+        private static List<int[]> FindElements(int N, int startIndex, int[] array, int targetSum)
+        {
+            List<int[]> result = new();
+            if (N == 2)
+            {
+                int i = startIndex, j = array.Length - 1;
+                while (i < j)
+                {
+                    var sum = array[i] + array[j];
+                    if (sum == targetSum)
+                    {
+                        result.Add(new int[2] { array[i], array[j] });
+                        i++;
+                        j--;
+                    }
+                    else if (sum < targetSum)
+                    {
+                        i++;
+                    }
+                    else
+                    {
+                        j--;
+                    }
+                }
+                return result;
+            }
+            for (int i = startIndex; i < array.Length; i++)
+            {
+                foreach (var nTuple in FindElements(N - 1, i + 1, array, targetSum - array[i]))
+                {
+                    result.Add((new[] { array[i] }.Concat(nTuple)).ToArray());
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Arrays/ArrayAlgorithms/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ArrayAlgorithms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[] { 12, 3, 1, 2, -6, 5, -8, 6 };
+            int targetSum = 0;
+            int N = 4;
+            
+            Console.WriteLine($"Finding {N}-elements having sum equal to {targetSum} in array [{string.Join(", ",array)}]:");
+            
+            foreach(var nTuple in NElementSum.FindElements(N, array, targetSum))
+            {
+                Console.WriteLine(string.Join(", ", nTuple));
+            }
+
+            Console.WriteLine("Finished");
+
+            Console.ReadKey();
+        }
+    }
+}
