87c77e8bedc5ec05196a91d7bbb79680dd871f97
Alireza Mokhtaripour <alireza.mokhtaripour@gmail.com>
2024-08-24 23:05:27 +1000

    Added four number sum

--- Arrays/ArrayAlgorithms/NElementSum.cs
+++ Arrays/ArrayAlgorithms/NElementSum.cs
@@ -16,30 +16,14 @@ namespace ArrayAlgorithms
 
         private static List<int[]> FindElements(int K, int startIndex, int[] array, int targetSum)
         {
-            List<int[]> result = new();
+            if (K == 4)
+                return FindFourNumberSum(startIndex, array, targetSum);
+
             if (K == 2)
-            {
-                int i = startIndex, j = array.Length - 1;
-                while (i < j)
-                {
-                    var sum = array[i] + array[j];
-                    if (sum == targetSum)
-                    {
-                        result.Add(new int[2] { array[i], array[j] });
-                        i++;
-                        j--;
-                    }
-                    else if (sum < targetSum)
-                    {
-                        i++;
-                    }
-                    else
-                    {
-                        j--;
-                    }
-                }
-                return result;
-            }
+                return FindTwoNumberSum(startIndex, array, targetSum);
+
+            List<int[]> result = new();
+
             for (int i = startIndex; i < array.Length; i++)
             {
                 foreach (var nTuple in FindElements(K - 1, i + 1, array, targetSum - array[i]))
@@ -50,5 +34,65 @@ namespace ArrayAlgorithms
             return result;
         }
 
+        private static List<int[]> FindTwoNumberSum(int startIndex, int[] array, int targetSum)
+        {
+            var result = new List<int[]>();
+            int i = startIndex, j = array.Length - 1;
+            while (i < j)
+            {
+                var sum = array[i] + array[j];
+                if (sum == targetSum)
+                {
+                    result.Add(new int[2] { array[i], array[j] });
+                    i++;
+                    j--;
+                }
+                else if (sum < targetSum)
+                {
+                    i++;
+                }
+                else
+                {
+                    j--;
+                }
+            }
+            return result;
+        }
+
+        private static List<int[]> FindFourNumberSum(int startIndex, int[] array, int targetSum)
+        {
+            var result = new List<int[]>();
+            var count = array.Length;
+            var pairs = new Dictionary<int, List<int[]>>();
+            for (int i = startIndex; i < count; i++)
+            {
+                for (int j = i + 1; j < count; j++)
+                {
+                    var thisSum = array[i] + array[j];
+                    var summand = targetSum - thisSum;
+                    if (pairs.ContainsKey(summand))
+                    {
+                        Console.WriteLine(thisSum + ":" + summand);
+                        foreach (var summandPair in pairs[summand])
+                        {
+                            result.Add(new int[]{
+                           array[i], array[j],
+                           summandPair[0], summandPair[1]
+                    });
+                        }
+                    }
+                }
+                for (int k = startIndex; k < i; k++)
+                {
+                    var sum = array[i] + array[k];
+                    if (!pairs.ContainsKey(sum))
+                    {
+                        pairs.Add(sum, new List<int[]>());
+                    }
+                    pairs[sum].Add(new int[] { array[i], array[k] });
+                }
+            }
+            return result;
+        }
     }
 }
--- Arrays/ArrayAlgorithms/Program.cs
+++ Arrays/ArrayAlgorithms/Program.cs
@@ -4,15 +4,17 @@
     {
         static void Main(string[] args)
         {
-            var array = new int[] { 12, 3, 1, 2, -6, 5, -8, 6 };
-            int targetSum = 0;
-            int K = 4;
-            
-            Console.WriteLine($"Finding {K}-elements having sum equal to {targetSum} in array [{string.Join(", ",array)}]:");
-            
-            foreach(var nTuple in NElementSum.FindElements(K, array, targetSum))
+            var array = new int[200];// { 12, 3, 1, 2, -6, 5, -8, 6 };
+            var rand = new Random(0);
+            array = array.Select(p => (int)rand.NextInt64(200)).ToArray();
+            int targetSum = 30;
+            int K = 6;
+
+            //Console.WriteLine($"Finding {K}-elements having sum equal to {targetSum} in array [{string.Join(", ", array)}]:");
+
+            foreach (var nTuple in NElementSum.FindElements(K, array, targetSum))
             {
-                Console.WriteLine(string.Join(", ", nTuple));
+                //Console.WriteLine(string.Join(", ", nTuple));
             }
 
             Console.WriteLine("Finished");
